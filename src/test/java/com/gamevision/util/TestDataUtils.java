be34a64539f8842442bbec2c2de84fbed355c4eb
Veyhayvey <vanyavoykova@gmail.com>
2022-08-12 19:13:47 +0300

    More integration tests remaining - GameControllerTest.

--- src/test/java/com/gamevision/util/TestDataUtils.java
+++ src/test/java/com/gamevision/util/TestDataUtils.java
@@ -4,8 +4,10 @@ import com.gamevision.model.entity.*;
 import com.gamevision.model.enums.GenreNameEnum;
 import com.gamevision.model.enums.UserRoleEnum;
 import com.gamevision.repository.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,14 +19,16 @@ public class TestDataUtils {
     private GenreRepository genreRepository;
     private PlaythroughRepository playthroughRepository;
     private CommentRepository commentRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TestDataUtils(UserRepository userRepository, UserRoleRepository userRoleRepository, GameRepository gameRepository, GenreRepository genreRepository, PlaythroughRepository playthroughRepository, CommentRepository commentRepository) {
+    public TestDataUtils(UserRepository userRepository, UserRoleRepository userRoleRepository, GameRepository gameRepository, GenreRepository genreRepository, PlaythroughRepository playthroughRepository, CommentRepository commentRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userRoleRepository = userRoleRepository;
         this.gameRepository = gameRepository;
         this.genreRepository = genreRepository;
         this.playthroughRepository = playthroughRepository;
         this.commentRepository = commentRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -38,14 +42,23 @@ public class TestDataUtils {
         }
     }
 
-    //no password
+    //No password in example test but seems necessary, since we use REAL repo in integration tests
+    //Example test was for Unit tests and hence with a MOCK repo!!!!
     public UserEntity createTestAdmin(String username) {
         initRoles();
         UserEntity admin = new UserEntity()
                 .setUsername(username)
                 .setEmail("admin@test.com")
+                .setPassword(passwordEncoder.encode("testAdminPass"))
                 .setActive(true)
-                .setUserRoles(new HashSet<>(userRoleRepository.findAll()));
+                .setUserRoles(new HashSet<>(userRoleRepository.findAll())) //admin gets all roles
+                .setGames(new HashSet<>())
+                .setFavouritePlaythroughs(new HashSet<>());
+
+
+        //todo try by 'specifying' the type of the set by adding an object to it
+        //  admin.getGames().add(createTestGame(admin));
+        //  admin.getFavouritePlaythroughs().add(createTestPlaythrough(admin));
 
         return userRepository.save(admin);
 
@@ -54,24 +67,34 @@ public class TestDataUtils {
 
     public UserEntity createTestUser(String username) {
         initRoles();
-        UserEntity admin = new UserEntity()
+        UserEntity user = new UserEntity()
                 .setUsername(username)
-                .setEmail("admin@test.com")
+                .setEmail("user@test.com") //don't forget to set a DIFFERENT pass after copying from admin...
+                .setPassword(passwordEncoder.encode("testUserPass"))
                 .setActive(true)
-                .setUserRoles(Set.of(new UserRoleEntity().setName(UserRoleEnum.USER)));
-
-        return userRepository.save(admin);
+                // .setUserRoles(Set.of(new UserRoleEntity().setName(UserRoleEnum.USER))); //not a new entity, get it from the repo where initRoles() saves them
+                .setUserRoles(Set.of(userRoleRepository.findByName(UserRoleEnum.USER).get()));
+        //.setGames(new HashSet<>())
+        //.setFavouritePlaythroughs(new HashSet<>());
+        System.out.println(user.toString());
+        //todo try by 'specifying' the type of the set by adding an object to it - shoud NOT be necessary
+        //   user.getGames().add(createTestGame(createTestAdmin("TestAdminWhoAddedATestGame")));
+        //   user.getFavouritePlaythroughs().add(createTestPlaythrough(createTestAdmin("TestAdminWhoAddedATestGame")));
+        return userRepository.save(user);
 
     }
 
-    public GameEntity createTestGame(UserEntity addedBy, Set<GenreEntity> genres) {
+    //TODO UNCOMMENT GAME AND PT
+    public GameEntity createTestGame(UserEntity addedBy) {
         GameEntity testGame = new GameEntity()
                 .setTitle("Test Game Title")
                 .setTitleImageUrl("Test Title Image URL")
                 .setDescription("Test game description")
-                .setGenres(Set.of(new GenreEntity().setName(GenreNameEnum.RPG),
-                        new GenreEntity().setName(GenreNameEnum.AA)))
-                .setPlaythroughs(Set.of(createTestPlaythrough(addedBy)))
+                .setGenres(Set.of(genreRepository.findByName(GenreNameEnum.RPG),
+                        genreRepository.findByName(GenreNameEnum.AA)))
+                .setAddedBy(addedBy) //todo check if forgetting this was flubbing the test
+                .setPlaythroughs(new HashSet<>())
+                //   .setPlaythroughs(Set.of(createTestPlaythrough(addedBy)))
                 .setComments(new HashSet<>());
         return gameRepository.save(testGame);
     }
@@ -80,16 +103,41 @@ public class TestDataUtils {
         PlaythroughEntity testPlaythrough = new PlaythroughEntity()
                 .setTitle("Test Playthrough Title")
                 .setVideoUrl("Test video URL")
-                .setDescription("Test Playthrough Description");
+                .setDescription("Test Playthrough Description")
+                .setAddedBy(addedBy);
 
         return playthroughRepository.save(testPlaythrough);
     }
 
-    public void wipeDatabase() {
-        userRepository.deleteAll();
+    public CommentEntity createTestComment(UserEntity author) {
+        CommentEntity testComment = new CommentEntity()
+                .setAuthor(author)
+                .setText("Test comment text 1.")
+                .setLikesCounter(0)
+                .setDateTimeCreated(LocalDateTime.of(2017, 7, 7, 7, 7));
+
+        return commentRepository.save(testComment);
+    }
+
+    //Genre repo shouldn't be necessary, it's hardcoded stuff
+
+
+    public void wipeDatabase() { //roles and genres are pretty much hardcoded, so @Autowired in the tests
+        //wiping order is important, due to FK constraints (playthrough tied to game, games have addedBy User)
+        commentRepository.deleteAll();
         playthroughRepository.deleteAll(); //to avoid orphaned playthroughs without a game
         gameRepository.deleteAll();
+        genreRepository.deleteAll();
+        userRepository.deleteAll();
+        userRoleRepository.deleteAll();
     }
+
+    // this.commentRepository.deleteAll();
+    //   //     this.playthroughRepository.deleteAll();
+    //   //     this.gameRepository.deleteAll();
+    //   //     this.genreRepository.deleteAll(); //todo
+    //   //     this.userRepository.deleteAll();
+    //   //     this.userRoleRepository.deleteAll();
 
 
 }
--- src/test/java/com/gamevision/web/GameControllerIT.java
+++ src/test/java/com/gamevision/web/GameControllerIT.java
@@ -1,38 +1,199 @@
 package com.gamevision.web;
 
-import com.gamevision.model.entity.*;
+import com.gamevision.model.entity.CommentEntity;
+import com.gamevision.model.entity.GameEntity;
+import com.gamevision.model.entity.PlaythroughEntity;
+import com.gamevision.model.entity.UserEntity;
 import com.gamevision.model.enums.GenreNameEnum;
+import com.gamevision.repository.GameRepository;
+import com.gamevision.repository.GenreRepository;
+import com.gamevision.repository.UserRoleRepository;
+import com.gamevision.service.GameService;
 import com.gamevision.util.TestDataUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Set;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+
 public class GameControllerIT {
 
-    @Autowired //autowire all entities, then assign to them the ready entities from TestDataUtils
-    private MockMvc mockMvc; //for sending HTTP requests to the test server TODO cannot autowire?!?
-    private TestDataUtils testDataUtils;
-    private UserEntity testUser, testAdmin;
-    private GenreEntity testGenre;
-    private PlaythroughEntity testPlaythrough;
-    private GameEntity testGame;
-    private CommentEntity testComment;
 
+    //autowire all necessary repos and services
+
+    //TODO: initialize test entities here for visibility
+    UserEntity testUser, testAdmin;
+    GameEntity testGame;
+    PlaythroughEntity testPlaythrough;
+    CommentEntity testComment;
+
+
+    //  @Autowired
+    //  private PlaythroughRepository playthroughRepository;
+    //  @Autowired
+    //  private CommentRepository commentRepository;
+    //roles and genres are hardcoded by nature, hence don't require additional data setup, no need to create anything more
+    @Autowired
+    private UserRoleRepository userRoleRepository;
+    @Autowired //hardcoded by nature, can be used as it is
+    private GenreRepository genreRepository;
+    //  @Autowired
+    //  private UserRepository userRepository;
+    @Autowired
+    private GameRepository gameRepository; //needed to check if empty for no games message
+    @Autowired
+    private GameService gameService; //for getGameByTitle, etc.
+    //or use the prepared TestDataUtils
+    @Autowired
+    private TestDataUtils testDataUtils;
+    //additional utils
+    @Autowired //autowire all necessary repos and services, then assign to them the ready entities from TestDataUtils
+    private MockMvc mockMvc; //for sending HTTP requests to the test server TODO cannot autowire?!?
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
-    void setUp() { //grab ready data from TestDataUtils and assign it to the test entities
-        testUser = testDataUtils.createTestUser("Game Controller Test User");
-        testAdmin = testDataUtils.createTestAdmin("Game Controller Test Admin");
+    public void setUp() { //saving to repos included
+        testUser = testDataUtils.createTestUser("TestUser");
+        testAdmin = testDataUtils.createTestAdmin("TestAdmin");
+        testGame = testDataUtils.createTestGame(testAdmin);
         testPlaythrough = testDataUtils.createTestPlaythrough(testAdmin);
-        testGame = testDataUtils.createTestGame(testAdmin, Set.of(testGenre.setName(GenreNameEnum.RPG), testGenre.setName(GenreNameEnum.AA)));
+        testComment = testDataUtils.createTestComment(testUser);
     }
+
+    // @AfterEach
+    // public void tearDown() {
+    //     this.commentRepository.deleteAll();
+    //     this.playthroughRepository.deleteAll();
+    //     this.gameRepository.deleteAll();
+    //     this.genreRepository.deleteAll();
+    //     this.userRepository.deleteAll();
+    //     this.userRoleRepository.deleteAll();
+    // }
+
+    @AfterEach
+    void tearDown() {
+        testDataUtils.wipeDatabase();
+    }
+
+
+    @Test
+    void allGamesViewShown() throws Exception {
+        this.mockMvc
+                .perform(get("/games/all"))
+                .andExpect(model().attributeExists("games")) //import static method
+                .andExpect(view().name("games-all"));
+    }
+
+    @Test
+    void displayNoGamesMessageWhenGameRepositoryIsEmpty() throws Exception {
+        gameRepository.deleteAll(); //will be hold the one test game created with setUp(), so clear it
+        this.mockMvc
+                .perform(get("/games/all"))
+                .andExpect(model().attributeExists("noGames")) //import static method
+                .andExpect(view().name("games-all"));
+    }
+
+    @Test
+    public void gameViewShown() throws Exception {
+        Long gameId = testGame.getId(); //Don't hardcode it here, work with the available test data!!!!!
+
+//Id should be 1
+        this.mockMvc
+                .perform(get("/games/" + gameId))
+                .andExpect(view().name("game"));
+    }
+
+    @Test
+    public void gameNotFound() throws Exception {
+        this.mockMvc
+                .perform(get("/games/1001"))
+                .andExpect(model().attribute("errorMessage", "Game not found."))
+                .andExpect(view().name("error"));
+    }
+
+
+    @Test
+    @WithMockUser(username = "TestAdmin", roles = "ADMIN") //don't forget the proper user or "No ModelAndView found"
+    public void addGameViewShown() throws Exception {
+        this.mockMvc
+                .perform(get("/games/add"))
+                .andExpect(model().attribute("allGenres", GenreNameEnum.values())) //the genres are shown with checkboxes
+                .andExpect(view().name("game-add"));
+
+    }
+
+
+    //TODO: POST addGameSubmit
+
+
+    //   @Test //constraint violation if no UserId
+    //@WithMockUser(username = "TestAdmin", roles = {"USER", "ADMIN"})
+    //  // @WithUserDetails(value="TestAdmin",  userDetailsServiceBeanName = "testUserDataService") //TODO USE THIS, see GitHub MobileleUserDetailsServiceTest.java
+    //   public void addGameWithValidData() throws Exception {
+    //       GameEntity existingGame = testGame; //this should create one game in the repo and actually fill the empty proxies
+    //       System.out.println("Existing game ID: " + existingGame.getId()); //id is 3
+    //       //Needs userId, but it's not contained in the mock
+//
+//
+//
+    //       Long existingGameId =  existingGame.getId(); // 8?  Maybe get the next? Is it random?
+    //   //    List<String> genres = new ArrayList<>(List.of("RPG", "AA"));
+    //       this.mockMvc                    //this is the input data from the BM - note how params are set; author is obviously not manually entered in the input input
+    //               .perform(post("/games/add")
+    //                       .param("title", "A Great Test Game") //give the values as a MAP KVP!!! Follow the BM field names for the KEYS.
+    //                       .param("titleImageUrl", "testurl")
+    //                      .param("description", "Test description that has to be long enough.") //should get it saved to repo
+    //               .param("genre", "RPG")  //singular in the controller due to checkbox naming issues
+    //               .with(csrf()))
+////Won't be saved by now?!? GAME NOT FOUND???
+////TODO find out how to test with more than one genre?
+    //               //genres come as a List<String>
+    //               // .with(csrf())) //that's for REST
+    //               .andExpect(redirectedUrl("/games/" + existingGameId + 1));
+    //             //  .andExpect(redirectedUrl("/games/" + gameService.getGameByTitle("A Great Test Game").getId()));//redirectedUrl, not view
+    //       //  Long gameId = gameService.getGameByTitle(testGame.getTitle()).getId();
+//
+//
+    //   }
+//
+    //todo: note the options we have
+    //  .andExpect(model().attribute  hasErrors, has a number of errors, does not exist
+
+//   @Test
+//   void testDeleteByAnonymousUser_Forbidden() throws Exception {
+//       mockMvc.perform(delete("/games/{id}/delete", testGame.getId())   //note the {id} param syntax with ,
+//               .with(csrf())
+//               ).
+//               andExpect(status().is3xxRedirection())
+//               .andExpect(redirectedUrl("/games/all"));
+//       //TODO: check redirection url to login w/o schema
+//   }
+
+//   @Test
+//   @WithMockUser(
+//           username = "admin@example.com",
+//           roles = {"ADMIN", "USER"}
+//   )
+//   void testDeleteByAdmin() throws Exception {
+//       mockMvc.perform(delete("/offers/{id}", testOffer.getId()).
+//                       with(csrf())
+//               ).
+//               andExpect(status().is3xxRedirection()).
+//               andExpect(view().name("redirect:/offers/all"));
+//   }
 
 
 }
